4ec7be55c7cedc3e15839c5fdebc5e0523cdd747
Tom Liu <gt3240@me.com>
2016-06-14 17:47:01 -0700

    Update Eva Entity Insert.sql

--- SQL/Eva Entity Insert.sql
+++ SQL/Eva Entity Insert.sql
@@ -13,30 +13,30 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE PROC [dbo].[EVA_Entity_Insert]
-			
-			@Name nvarchar(255)
-           ,@Slug nvarchar(255)
-           ,@WebsiteId int
-		   ,@OID int output
+
+	@Name nvarchar(255)
+       ,@Slug nvarchar(255)
+       ,@WebsiteId int
+       ,@OID int output
 
 AS
 
 BEGIN
 INSERT INTO [dbo].[EVA_Entities]
            
-		   (
-		    [Name]
-           ,[Slug]
-           ,[CreatedDate]
-           ,[WebsiteId]
-		   )
+	(
+	[Name]
+       ,[Slug]
+       ,[CreatedDate]
+       ,[WebsiteId]
+	)
      VALUES
-           (
-		    @Name
-           ,@Slug
-		   , GETUTCDATE()
-           ,@WebsiteId
-		   )
+        (
+	@Name
+       ,@Slug
+       ,GETUTCDATE()
+       ,@WebsiteId
+        )
 
 SET @OID = SCOPE_IDENTITY()
 END
